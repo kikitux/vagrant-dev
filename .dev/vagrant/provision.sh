2352327956539dbccc8023c097f202c6984ef73c
Benjamin Hinchley <benjaminhinchley@gmail.com>
2016-02-29 17:02:40 +1100

    add dotfiles and chown $GOPATH
    Signed-off-by: Benjamin Hinchley <benjaminhinchley@gmail.com>

--- .dev/vagrant/provision.sh
+++ .dev/vagrant/provision.sh
@@ -8,7 +8,7 @@ exec 2>&1
 
   sudo rm /var/lib/apt/lists/* -vrf
 
-  PACKAGES="curl git bzr"
+  PACKAGES="curl git bzr cmake vim-nox python-dev ctags"
   sudo -E -H apt-get update
   sudo -E -H apt-get install -y -q --no-install-recommends ${PACKAGES}
 
@@ -34,5 +34,13 @@ mkdir -p $GOPATH
   go get github.com/Masterminds/glide
   cd $GOPATH/src/github.com/Masterminds/glide && make bootstrap && make build && make install
 }
+
+[ -d /home/vagrant/dotfiles ] || {
+  mkdir -p /home/vagrant/dotfiles && cd $_
+  git clone https://github.com/benhinchley/vagrant-dotfiles.git .
+  sh setup.sh
+}
+
+cd $GOPATH && sudo chown -R vagrant .
 
 echo "everything should be ready to dev"
